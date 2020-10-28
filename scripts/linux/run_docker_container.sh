1e9ca6185edabc07eb05316daa1ee44cdca9cbbd
Marios Karagiannopoulos from Jenkins <marios.karagiannopoulos@loctio.com>
2020-10-28 20:29:25 +0200

    Fixing MAC address and docker service name

--- scripts/linux/run_docker_container.sh
+++ scripts/linux/run_docker_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-MAC_ADDRESS=$(ip link show eth0 | awk '/ether/ {print $2}')
+MAC_ADDRESS=$(ip link show ens32 | awk '/ether/ {print $2}')
 echo "MAC_ADDRESS=${MAC_ADDRESS}" > .env-cloud
 echo "CLOUD_API_HOST=loctio-api.ddns.net" >> .env-cloud
 echo "ECR=810204744368.dkr.ecr.eu-central-1.amazonaws.com" >> .env-cloud
 
 docker-compose --env-file .env-cloud -f docker-compose.yml down
-docker-compose --env-file .env-cloud -f docker-compose.yml --compatibility up -d loctio_ue
+docker-compose --env-file .env-cloud -f docker-compose.yml --compatibility up -d loctio_ue_x86_64
 docker logs -f loctio_ue
