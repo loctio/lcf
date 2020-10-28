52dcacbd6d6a203f5a541fc80538be04494a8c1b
LoctioLabs <62446975+loctio@users.noreply.github.com>
2020-10-28 19:27:09 +0200

    Update loctio_ue_raspbian.sh

--- scripts/raspbian/loctio_ue_raspbian.sh
+++ scripts/raspbian/loctio_ue_raspbian.sh
@@ -1,6 +1,4 @@
 AWS_ID="810204744368" AWS_REGION="eu-central-1"
 AWS_ECR_DOMAIN="$AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 AWS_ACCESS_KEY_ID=AKIA3ZI7XJKYLF25JANE
-AWS_SECRET_ACCESS_KEY=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 aws ecr get-login --
-region eu-central-1 --no-include-email > aws_login.sh && chmod +x aws_login.sh && ./aws_login.sh; docker
-pull $AWS_ECR_DOMAIN/loctio_ue_arm:latest
+AWS_SECRET_ACCESS_KEY=EhZ89doYkAQtvgiSPYjMaY2mFyX0IHpBTSogOtnj aws ecr get-login --region eu-central-1 --no-include-email > aws_login.sh && chmod +x aws_login.sh && ./aws_login.sh; docker pull $AWS_ECR_DOMAIN/loctio_ue_arm:latest
