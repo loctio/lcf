a4a98910fc001adbd0245d6f75c0b7bf50dace70
LoctioLabs <62446975+loctio@users.noreply.github.com>
2020-10-29 19:27:44 +0200

    Update loctio_ue_linux.sh

--- scripts/linux/loctio_ue_linux.sh
+++ scripts/linux/loctio_ue_linux.sh
@@ -1,4 +1,10 @@
-AWS_ID="810204744368" AWS_REGION="eu-central-1"
+AWS_ID="810204744368"
+AWS_REGION="eu-central-1"
 AWS_ECR_DOMAIN="$AWS_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 AWS_ACCESS_KEY_ID=AKIA3ZI7XJKYLF25JANE
-AWS_SECRET_ACCESS_KEY=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 aws ecr get-login --region eu-central-1 --no-include-email > aws_login.sh && chmod +x aws_login.sh && ./aws_login.sh; docker pull $AWS_ECR_DOMAIN/loctio_ue_x86_64:latest
+AWS_SECRET_ACCESS_KEY=EhZ89doYkAQtvgiSPYjMaY2mFyX0IHpBTSogOtnj 
+
+aws configure set profile.loctio.region $AWS_REGION
+aws configure set profile.loctio.aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set profile.loctio.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws --profile loctio ecr get-login --no-include-email > aws_login.sh && chmod +x aws_login.sh && ./aws_login.sh; docker pull $AWS_ECR_DOMAIN/loctio_ue_x86_64:latest
