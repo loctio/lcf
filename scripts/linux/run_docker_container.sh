85d55b41c1b9c5ec6953d8d26fdcd35a448ef75e
Marios Karagiannopoulos <mariosk@gmail.com>
2020-10-30 23:11:12 +0200

    Update run_docker_container.sh

--- scripts/linux/run_docker_container.sh
+++ scripts/linux/run_docker_container.sh
@@ -3,7 +3,7 @@
 ETHIF="ens32"
 MAC_ADDRESS=$(ip link show $ETHIF | awk '/ether/ {print $2}')
 echo "MAC_ADDRESS=${MAC_ADDRESS}" > .env-cloud
-echo "CLOUD_API_HOST=loctio-api.ddns.net" >> .env-cloud
+echo "CLOUD_API_HOST=api.loctio.com" >> .env-cloud
 echo "ECR=810204744368.dkr.ecr.eu-central-1.amazonaws.com" >> .env-cloud
 
 docker-compose --env-file .env-cloud -f docker-compose.yml down
