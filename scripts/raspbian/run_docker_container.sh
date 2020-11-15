7dacc822f9411770f40036b7f3cecba905b34076
Marios Karagiannopoulos <mariosk@gmail.com>
2020-11-15 10:31:21 +0200

    Update run_docker_container.sh

--- scripts/raspbian/run_docker_container.sh
+++ scripts/raspbian/run_docker_container.sh
@@ -15,7 +15,7 @@ echo "RTL_SDR_BUS=${BUS_VALUE}" >> .env-cloud
 echo "CLOUD_API_HOST=api.loctio.com" >> .env-cloud
 echo "ECR=810204744368.dkr.ecr.eu-central-1.amazonaws.com" >> .env-cloud
 
-loctio_ue_raspbian.sh
+./loctio_ue_raspbian.sh
 docker-compose --env-file .env-cloud -f docker-compose.yml down
 docker-compose --env-file .env-cloud -f docker-compose.yml --compatibility up -d loctio_ue_arm
 docker logs -f loctio_ue
